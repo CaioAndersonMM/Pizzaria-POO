0785baf930ec6b6b8e1efc3b190699cfce860ee4
thiagocoelhoo <thiago.coelho158@gmail.com>
2023-08-08 18:28:58 -0300

    feat: adicionar setters na classe Produto

--- Source/Produto.java
+++ Source/Produto.java
@@ -1,23 +1,41 @@
 public class Produto {
-    private final String nomeProduto;
-    private final int quatidadeProduto;
-    private final float valor;
+    private String nomeProduto;
+    private int quatidadeProduto;
+    private float valor;
 
-    public Produto(String nomeProduto, int quatidadeProduto, float valor) {
-        this.nomeProduto = nomeProduto;
-        this.quatidadeProduto = quatidadeProduto;
-        this.valor = valor;
+    public Produto(String nomeProduto, int quatidadeProduto, float valor){
+        setNomeProduto(nomeProduto);
+        setQuantidadeProduto(quatidadeProduto);
+        setValor(valor);
     }
 
     public String getNomeProduto() {
         return nomeProduto;
+    } 
+    public void setNomeProduto(String nome) {
+        if (nome == null || nome.isEmpty()) {
+            throw new IllegalArgumentException("O nome não pode ser nulo ou uma string vazia.");
+        }
+        this.nomeProduto = nome;
     }
 
     public int getQuatidadeProduto() {
         return quatidadeProduto;
     }
+    public void setQuantidadeProduto(int quantidade) {
+        if (quantidade < 0) {
+            throw new IllegalArgumentException("Não é possível criar um produto com quantidade negativa.");
+        }
+        this.quatidadeProduto = quantidade;
+    }
 
-    public Float getValor() {
+    public float getValor() {
         return valor;
+    }
+    public void setValor(float valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException("O valor do produto não pode ser negativo.");
+        }
+        this.valor = valor;
     }
 }
