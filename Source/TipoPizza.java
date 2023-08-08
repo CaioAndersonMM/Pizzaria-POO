e9d3032942611467948d45af62640704636e17f1
thiagocoelhoo <thiago.coelho158@gmail.com>
2023-08-08 19:08:45 -0300

    feat: implementar setters  da classe TipoPizza

--- Source/TipoPizza.java
+++ Source/TipoPizza.java
@@ -1,26 +1,66 @@
 import java.util.Map;
-import java.util.Objects;
 
 public class TipoPizza {
     private String nomeSabor;
     private Map<Produto, Integer> ingredientes;
-    private float valores;
+    private float[] valores;
 
-    public TipoPizza(String nomeSabor, Map<Produto, Integer> ingredientes, float valores) {
-        this.nomeSabor = nomeSabor;
-        this.ingredientes = ingredientes;
-        this.valores = valores;
+    public TipoPizza(String nomeSabor, Map<Produto, Integer> ingredientes, float[] valores) {
+        setNomeSabor(nomeSabor);
+        setIngredientes(ingredientes);
+        setValores(valores);
     }
 
-    public float getValores() {
+    public float[] getValores() {
         return valores;
+    }
+    public void setValores(float[] valores) {
+        // Verificar se foram informados três valores
+        if (valores == null || valores.length != 3) {
+            throw new IllegalArgumentException("Preços inválidos.");
+        }
+
+        // Verificar se há algum valor negativo
+        for (int i = 0; i < 3; i++) {
+            if (valores[i] < 0) {
+                throw new IllegalArgumentException("Não é possível definir um preço negativo.");
+            }
+        }
+
+        // Copiar array de float
+        this.valores = new float[valores.length];
+        System.arraycopy(valores, 0, this.valores, 0, valores.length);
     }
 
     public Map<Produto, Integer> getIngredientes() {
-        return ingredientes;
+        return Map.copyOf(this.ingredientes);
+    }
+    public void setIngredientes(Map<Produto, Integer> ingredientes) {
+        this.ingredientes = Map.copyOf(ingredientes);
+    }
+    public void addIngrediente(Produto ingrediente, Integer quantidade) {
+        // Verificar se os valores informados são válidos
+        if (ingrediente == null) {
+            throw new IllegalArgumentException("O ingrediente não pode ser nulo.");
+        }
+        if (quantidade < 0) {
+            throw new IllegalArgumentException("Não é possível adicionar um produto com quantidade negativa.");
+        }
+
+        // Adicionar ingrediente
+        this.ingredientes.put(ingrediente, quantidade);
+    }
+    public void removeIngrediente(Produto ingrediente) {
+        this.ingredientes.remove(ingrediente);
     }
 
     public String getNomeSabor() {
         return nomeSabor;
+    }
+    public void setNomeSabor(String nomeSabor) {
+        if (nomeSabor == null || nomeSabor.isEmpty()) {
+            throw new IllegalArgumentException("O nome do sabor não pode ser nulo ou vazio.");
+        }
+        this.nomeSabor = nomeSabor;
     }
 }
