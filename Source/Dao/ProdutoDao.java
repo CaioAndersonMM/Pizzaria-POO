efdab554a1475831494c6582bce8c81eb67e3d1e
mikael-johnatan <138150103+mikael-johnatan@users.noreply.github.com>
2023-09-09 17:32:47 -0300

    Update ProdutoDao.java
    Atualização dos métodos inserir , alterar , deletar e buscar da Classe ProdutoDao

--- Source/Dao/ProdutoDao.java
+++ Source/Dao/ProdutoDao.java
@@ -8,32 +8,119 @@ public class ProdutoDao extends BaseDaoImp<Produto> {
 
     @Override
     public void alterar(Produto entity) {
-        // TODO Auto-generated method stub
-
+          String sql = "UPDATE Produto SET nome=?, quantidade=?, valor=? WHERE id=?";
+        try {
+            Connection con = getConnection(); 
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, entity.getNomeProduto());
+            stmt.setInt(2, entity.getQuatidadeProduto());
+            stmt.setFloat(3, entity.getValor());
+            stmt.setLong(4, entity.getId()); 
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection(); 
+        }
     }
-
     @Override
     public Produto buscar(Produto entity) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+                String sql = "SELECT * FROM produto as e WHERE e.nome = ? ";
+            try {
+                PreparedStatement stmt = connection.prepareStatement(sql);
+                stmt.setString(1, entity.getNomeProduto()); // Substitua 1 pelo número do parâmetro correspondente à coluna "nome" na consulta SQL.
+                ResultSet rs = stmt.executeQuery();
 
+                if (rs.next()) {
+                    // Crie um objeto Produto a partir dos dados do ResultSet e retorne-o
+                    Produto produto = new Produto();
+                    produto.setNomeProduto(rs.getString("nome")); 
+                    produto.setQuantidadeProduto(rs.getInt("quantidade")); 
+                    produto.setValor(rs.getFloat("valor")); 
+                    return produto;
+                } else {
+                    return null;
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                return null;
+            } finally {
+                closeConnection();
+            }
+        }
+    
     @Override
     public void deletar(Produto entity) {
-        // TODO Auto-generated method stub
-
+ 
+        String sql = "DELETE FROM Produto WHERE id=?";
+        try {
+            Connection con = getConnection(); 
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setLong(1, entity.getId());
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+        
+        e.printStackTrace();
+        } finally {
+        closeConnection();
+        }
     }
+
 
     @Override
     public Long inserir(Produto entity) {
-        // TODO Auto-generated method stub
+   
+     C String sql = "INSERT INTO Produto (nome, quatidade, valor) " 
+            + "values (?,?,?)";
+    try {
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, entity.getNomeProduto());
+        ps.setInt(2, entity.getQuatidadeProduto());
+        ps.setFloat(3, entity.getValor());
+        ps.execute();
+        ps.close();
+
+        sql = "SELECT * FROM Produto as e WHERE e.nomeProduto=?";
+        ps = con.prepareStatement(sql);
+        ps.setString(1, entity.getNomeProduto());
+        ResultSet rs = ps.executeQuery();
+        if (rs.next())
+            return rs.getLong("id");
+        else
+            return null;
+
+    } catch (SQLException e) {
+        e.printStackTrace();
         return null;
+    } finally {
+        closeConnection();
     }
+}
 
     @Override
     public List listar() {
-        // TODO Auto-generated method stub
-        return null;
+              String sql = "SELECT * FROM Produtos";
+            List<Produto> listProd = new ArrayList<>();
+            try {
+                PreparedStatement stmt = connection.prepareStatement(sql);
+                ResultSet resultado = stmt.executeQuery();
+                while (resultado.next()) {
+                    Produto produto = new Produto();
+                    produto.setNomeProduto(resultado.getNomeProduto("nome"));
+                    produto.getQuatidadeProduto(resultado.getQuatidadeProduto("quantidade"));
+                    produto.setValor(resultado.getValor("valor"));
+                    listProd.add(produto);
+                }
+                return listProd;
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                return null;
+            } finally {
+                closeConnection();
+            }
+        }
     }
 
-}
+
