177c69f7e80a68c3783072aa6e8788d50fe106ed
Thiago Coelho <thiago.coelho158@gmail.com>
2023-09-09 20:42:10 -0300

    Fix: adicionar atributos nome, cpf e id a Cliente

--- Source/Model/Entity/Cliente.java
+++ Source/Model/Entity/Cliente.java
@@ -1,7 +1,10 @@
 package Model.Entity;
 
 public class Cliente extends Usuario {
+    private Long id;
     private String endereco;
+    private String nome;
+    private String cpf;
 
     public Cliente() {
     }
@@ -24,8 +27,39 @@ public class Cliente extends Usuario {
         return endereco;
     }
 
-    public void alterarEndereco(String novoendereco) {
-        setEndereco(novoendereco);
+    public void setNome(String nome) {
+        if (nome == null || nome.isEmpty()) {
+            return;
+        }
+
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setCpf(String cpf) {
+        if (cpf == null || cpf.length() < 11) {
+            return;
+        }
+
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setId(Long id) {
+        if (id == null || id < 1) {
+            return;
+        }
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
     }
 
     // Esses métodos serão sobrescritos
