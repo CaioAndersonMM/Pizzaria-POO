439b7d64eaabd5a78176575e88b82abe1a5dc415
mikael-johnatan <138150103+mikael-johnatan@users.noreply.github.com>
2023-09-09 22:29:46 -0300

    Update GerenteDao.java
    implementação dos métodos da classe GerenteDao.

--- Source/Dao/GerenteDao.java
+++ Source/Dao/GerenteDao.java
@@ -7,32 +7,128 @@ public class GerenteDao extends BaseDaoImp<Gerente> {
 
     @Override
     public void alterar(Gerente entity) {
-        // TODO Auto-generated method stub
 
-    }
+        String sql = "UPDATE Gerente\n" + 
+                    "SET nome = ?, endereco = ?, cpf = ?\n" + 
+                    "WHERE id = ?;";
+    
+            try {
+                Connection con = getConnection();
+                PreparedStatement stmt = con.prepareStatement(sql);
 
+                stmt.setString(1, entity.getNome());
+                stmt.setString(2, entity.getEndereco());
+                stmt.setString(3, entity.getCpf());
+                stmt.setLong(4, entity.getId());
+
+                stmt.execute();
+                stmt.close();
+            } catch(SQLException e) {
+                e.printStackTrace();
+            } finally {
+                closeConnection();
+            }
+        }
+    
     @Override
     public Gerente buscar(Gerente entity) {
-        // TODO Auto-generated method stub
+    String sql = "SELECT * FROM Gerente as e WHERE e.id = ?";
+
+        try {
+            Connection con = getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setLong(1, entity.getId());
+
+            ResultSet rs = stmt.executeQuery();
+            stmt.close();
+
+            if (rs.next()) {
+                Gerente gerente = new Gerente();
+                return gerente;
+            }
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+    
         return null;
     }
 
     @Override
     public void deletar(Gerente entity) {
         // TODO Auto-generated method stub
+        String sql = "DELETE FROM Gerente as e WHERE e.cpf = ?";
 
+        try {
+            Connection con = getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, entity.getCpf());
+            stmt.execute();
+            stmt.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
     }
+    
 
     @Override
     public Long inserir(Gerente entity) {
-        // TODO Auto-generated method stub
-        return null;
+        String sql = "UPDATE Gerente\n" + //
+                "SET nome = ?, endereco = ?, cpf = ?\n" + //
+                "WHERE id = ?;";
+
+        try {
+            Connection con = getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setString(1, entity.getNome());
+            stmt.setString(2, entity.getEndereco());
+            stmt.setString(3, entity.getCpf());
+            stmt.setLong(4, entity.getId());
+
+            stmt.execute();
+            stmt.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
     }
+        
 
     @Override
-    public List listar() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+     public List<Gerente> listar() {
+            String sql = "SELECT * FROM Gerente";
+            List<Gerente > gerente = new ArrayList<Gerente >();
+    
+            try {
+                Connection con = getConnection();
+                PreparedStatement stmt = con.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery();
+                stmt.close();
 
-}
+                while (rs.next()) {
+                    Gerente gerente = new Gerente();
+                    try {
+                        gerente.setId(rs.getLong("id"));
+                        gerente.setCpf(rs.getString("cpf"));
+                        gerente.setEndereco(rs.getString("endereco"));
+                        gerente.setNome(rs.getString("nome"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    gerente.add(gerente);
+                }
+            } catch(SQLException e) {
+                e.printStackTrace();
+            } finally {
+                closeConnection();
+            }
+
+            return gerente;
+        }
+   
