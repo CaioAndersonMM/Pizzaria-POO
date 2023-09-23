26a9029002d692a4ad5bc079403d795ad10b33fb
CaioAndersonMM <caioandersonmartins@gmail.com>
2023-09-23 19:43:42 -0300

    Adicionado fxml de Cliente

--- Source/View/HelloFx.java
+++ Source/View/HelloFx.java
@@ -1,30 +1,14 @@
 package View;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-
 
 public class HelloFx extends Application{
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginGrande.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Cliente.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
