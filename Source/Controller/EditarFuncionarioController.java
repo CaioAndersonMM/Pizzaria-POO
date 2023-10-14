af8d9884be3f9d34284b167978beb5606593e257
CaioAndersonMM <caioandersonmartins@gmail.com>
2023-10-14 12:34:20 -0300

    Editar Funcionário Corrigido

--- Source/Controller/EditarFuncionarioController.java
+++ Source/Controller/EditarFuncionarioController.java
@@ -1,9 +1,14 @@
 package Controller;
 
+import Model.BO.FuncionarioBo;
+import Model.Entity.Funcionario;
+import View.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class EditarFuncionarioController {
     @FXML
@@ -11,9 +16,6 @@ public class EditarFuncionarioController {
 
     @FXML
     private Button salvar;
-
-    @FXML
-    private TextField confirm_senha;
 
     @FXML
     private TextField nome;
@@ -30,18 +32,31 @@ public class EditarFuncionarioController {
         this.id = id;
         nome.setText(nomefuncionario);
         senha.setText(senhafuncionario);
-        confirm_senha.setText(senhafuncionario);
         cpf.setText(cpffuncionario);
     }
 
     @FXML
-    void salvar(ActionEvent event) {
-        
+    void salvar(ActionEvent event) throws Exception {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setCPF(cpf.getText());
+        funcionario.setNome(nome.getText());
+        funcionario.setSenha(senha.getText());
+        funcionario.setId(id);
+
+        FuncionarioBo bo = new FuncionarioBo();
+        bo.editar(id, funcionario);
+
+        // Obter o Stage do modal e fechá-lo.
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+        //Melhor chamar a tela denovo para recarregar!
+        App.telaFuncionarios();
     }
 
     @FXML
     void cancelar(ActionEvent event) {
-
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 
 }
