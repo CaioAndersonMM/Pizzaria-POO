4e78bb299b20f1fd6a65e32c715e9bee873e0663
CaioAndersonMM <caioandersonmartins@gmail.com>
2023-10-15 15:03:34 -0300

    Nova Consulta para o Relatório Financeiro e Utilização da Função

--- Source/Dao/DetalhesPedidoDAO.java
+++ Source/Dao/DetalhesPedidoDAO.java
@@ -58,10 +58,8 @@ public class DetalhesPedidoDAO {
                     detalhes.setNomeCliente(rs.getString("nome_cliente"));
                     detalhes.setSabor(rs.getString("sabor"));
 
-                    // detalhes.setCpfCliente(rs.getString("cpf_cliente"));
-                    // detalhes.setNomeFuncionario(rs.getString("nome_funcionario"));
-
                     detalhesPedidos.add(detalhes);
+
                 }
             }
         } catch (SQLException e) {
@@ -69,4 +67,48 @@ public class DetalhesPedidoDAO {
         }
         return detalhesPedidos;
     }
+
+    public List<DetalhesPedido> obterDetalhesFinanceiros() {
+        List<DetalhesPedido> detalhesPedidos = new ArrayList<>();
+        try {
+            BaseDaoImp.connection = BaseDaoImp.getConnection();
+            String consultaSQL = "SELECT * FROM vw_detalhes_financeiro";
+            try (PreparedStatement stmt = connection.prepareStatement(consultaSQL);
+                 ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+
+                    DetalhesPedido detalhes = new DetalhesPedido();
+
+                    detalhes.setGanho(rs.getDouble("ganhos"));
+                    detalhes.setGasto(rs.getDouble("gastos"));
+
+                    detalhesPedidos.add(detalhes);
+
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return detalhesPedidos;
+    }
+    
+    public double lucro() {
+        double lucro = 0.0;
+        try {
+            BaseDaoImp.connection = BaseDaoImp.getConnection();
+            String consultaSQL = "SELECT calcular_lucro() as lucro";
+            try (PreparedStatement stmt = connection.prepareStatement(consultaSQL);
+                ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+
+                    lucro = rs.getDouble("lucro");
+
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lucro;
+    }
+    
 }
