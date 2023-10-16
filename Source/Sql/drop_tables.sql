924a59d646890a3550ae5afde27ea7f8df92ada0
CaioAndersonMM <caioandersonmartins@gmail.com>
2023-10-15 23:20:46 -0300

    Drop Table com Mais Comandos

--- Source/Sql/drop_tables.sql
+++ Source/Sql/drop_tables.sql
@@ -1,4 +1,6 @@
 DROP VIEW vw_detalhes_pedidos;
+DROP VIEW vw_detalhes_pedidos2;
+DROP VIEW vw_detalhes_financeiro;
 
 DROP TABLE tb_pedidos_pizzas;
 DROP TABLE tb_pizzas_adicionais;
@@ -10,3 +12,9 @@ DROP TABLE tb_clientes;
 DROP TABLE tb_funcionarios;
 DROP TABLE tb_tipos_pizzas;
 DROP TABLE tb_produtos;
+
+
+DROP FUNCTION calcular_lucro;
+DROP FUNCTION atualizar_vendas_apos_insercao_pedido;
+DROP FUNCTION criar_venda_apos_insercao_pizza_pedido;
+DROP FUNCTION criar_venda_apos_insercao_produto;
